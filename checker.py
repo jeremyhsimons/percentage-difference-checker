5db2258dcfc334f6e8d9521c8e8002268ecd4f8d
jeremyhsimons <>
2023-07-07 11:10:34 +0000

    Fix indentation errors.

--- checker.py
+++ checker.py
@@ -11,7 +11,7 @@ def get_threshold():
     THRESHOLD global variable.
     """
     threshold_input = input("PLEASE ENTER THE THRESHOLD FOR SIGNIFICANT DIFFERENCE HERE: \n")
-    threshold_float = int(threshold_input) / 100
+    threshold_float = float(threshold_input) / 100
     return threshold_float
 
 
@@ -21,23 +21,24 @@ def check_data(threshold):
     and for each row checks all the float values against
     the national average column.
     """
+    print("function call working")
     df = pd.read_excel('poll-data.xlsx', sheet_name="Full Results")
     for index in range(len(df)):
-    national_average = (df.iloc[index, 2])
-    if isinstance(national_average, float):
-        i = 3
-        while i <= 31:
-            data = df.iloc[index, i]
-            if isinstance(data, float):
-                if data > national_average + threshold:
-                    print(f"{index} outlier, greater than")
-                elif data < national_average - threshold:
-                    print(f"{index} outlier, less than")
-            else:
-                pass
-            i += 1
-    else:
-        pass
+        national_average = (df.iloc[index, 2])
+        if isinstance(national_average, float):
+            i = 3
+            while i <= 31:
+                data = df.iloc[index, i]
+                if isinstance(data, float):
+                    if data > national_average + threshold:
+                        print(f"{index} outlier, greater than")
+                    elif data < national_average - threshold:
+                        print(f"{index} outlier, less than")
+                else:
+                    pass
+                i += 1
+        else:
+            pass
 
 
 def main():
