74145798ebdc4d3fa78e9dabc0c72e41c91d64c9
jeremyhsimons <>
2023-07-07 10:54:20 +0000

    refactor checking code into func managed by main()

--- checker.py
+++ checker.py
@@ -3,7 +3,6 @@ import pandas as pd
 import openpyxl
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-THRESHOLD = 0
 
 def get_threshold():
     """
@@ -12,36 +11,39 @@ def get_threshold():
     THRESHOLD global variable.
     """
     threshold_input = input("PLEASE ENTER THE THRESHOLD FOR SIGNIFICANT DIFFERENCE HERE: \n")
-    threshold = int(threshold_input) / 100
-    return threshold
+    threshold_float = int(threshold_input) / 100
+    return threshold_float
 
 
-def main():
-    THRESHOLD = get_threshold()
-    print(f"You have selected the threshold of: {THRESHOLD * 100}% difference.")
-
-# main()
-
-df = pd.read_excel('poll-data.xlsx', sheet_name="Full Results")
-test = df.head(20)
-
-for index in range(len(df)):
+def check_data(threshold):
+    """
+    A function that loops through all rows of the data
+    and for each row checks all the float values against
+    the national average column.
+    """
+    df = pd.read_excel('poll-data.xlsx', sheet_name="Full Results")
+    for index in range(len(df)):
     national_average = (df.iloc[index, 2])
     if isinstance(national_average, float):
         i = 3
         while i <= 31:
             data = df.iloc[index, i]
             if isinstance(data, float):
-                if data > national_average + THRESHOLD:
-                    print("outlier, greater than")
-                elif data < national_average - THRESHOLD:
-                    print("outlier, less than")
+                if data > national_average + threshold:
+                    print(f"{index} outlier, greater than")
+                elif data < national_average - threshold:
+                    print(f"{index} outlier, less than")
             else:
                 pass
             i += 1
     else:
         pass
-    
 
 
-# print(df.head(20))
+def main():
+    threshold = get_threshold()
+    print(f"You have selected the threshold of: {threshold * 100}% difference.")
+    input("Press Enter to run the checker with this threshold.")
+    check_data(threshold)
+
+main()
