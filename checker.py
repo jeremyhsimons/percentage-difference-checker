f72cb2192cea66575670a59bb3e08fc536b20ed4
jeremyhsimons <>
2023-07-13 09:32:09 +0000

    Add a function to find cell coords rather than hardcoding.

--- checker.py
+++ checker.py
@@ -3,6 +3,10 @@ import pandas as pd
 import openpyxl
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+df = pd.read_excel('poll_data.xlsx', sheet_name="Full Results")
+
+TOTAL_CELL = []
+
 OUTPUT_TABLE = {
     "Excel table row number" : [],
     "Question name" : [],
@@ -12,16 +16,19 @@ OUTPUT_TABLE = {
     "Significant difference (%)": []
 }
 
+
 def get_threshold():
     """
     A function that collects the threshold for a significant difference
     (from the national average) and stores it in the 
     THRESHOLD global variable.
     """
-    threshold_input = input("PLEASE ENTER THE THRESHOLD FOR SIGNIFICANT DIFFERENCE HERE: \n")
+    threshold_input = input(
+        "PLEASE ENTER THE THRESHOLD FOR SIGNIFICANT DIFFERENCE HERE: \n"
+        )
     try:
         if not isinstance(float(threshold_input), float):
-            raise ValueError (
+            raise ValueError(
                 "Please enter a valid number"
             )
         else:
@@ -32,6 +39,22 @@ def get_threshold():
         return False
 
 
+def find_total_cell():
+    """
+    A function that loops through rows and columns 
+    and returns the index for each.
+    """
+    df = pd.read_excel('poll_data.xlsx', sheet_name="Full Results")
+    for index in range(len(df)):
+        row = df.iloc[index]
+        j = 0
+        while j < row.shape[0]:
+            if df.iloc[index, j] == 'Total':
+                return [index, j]
+            else:
+                j += 1
+
+
 def scan_data(threshold):
     """
     A function that loops through all rows of the data
@@ -39,14 +62,14 @@ def scan_data(threshold):
     is sent to the checking function
     """
     df = pd.read_excel('poll_data.xlsx', sheet_name="Full Results")
-    for index in range(len(df)):
-        national_average = (df.iloc[index, 2])
+    for row in range(len(df)):
+        national_average = (df.iloc[row, TOTAL_CELL[1]])
         if isinstance(national_average, float):
-            i = 3
-            while i <= 31:
-                data = df.iloc[index, i]
+            i = TOTAL_CELL[1] + 1
+            while i <= df.iloc[row].shape[0] - 1:
+                data = df.iloc[row, i]
                 if isinstance(data, float):
-                    check_data(df, index, i, data, national_average, threshold)
+                    check_data(df, row, i, data, national_average, threshold)
                 else:
                     pass
                 i += 1
@@ -74,7 +97,7 @@ def add_to_output(df, index, i, data, national_average, threshold):
     """
     OUTPUT_TABLE["Excel table row number"].append(index + 2)
     OUTPUT_TABLE["Question name"].append(df.iloc[index, 1])
-    OUTPUT_TABLE["Crossbreak subgroup"].append(df.iloc[4, i])
+    OUTPUT_TABLE["Crossbreak subgroup"].append(df.iloc[TOTAL_CELL[0], i])
     OUTPUT_TABLE["National average for question (%)"].append(national_average * 100)
     OUTPUT_TABLE["Proportion for subgroup (%)"].append(data * 100)
     OUTPUT_TABLE["Significant difference (%)"].append((data - national_average) * 100)
@@ -87,6 +110,9 @@ def main():
         return
     print(f"You have selected the threshold of: {threshold * 100}% difference.")
     input("Press 'Enter' to run the checker with this threshold.")
+    total_cell = find_total_cell()
+    TOTAL_CELL.append(total_cell[0])
+    TOTAL_CELL.append(total_cell[1])
     scan_data(threshold)
     output = pd.DataFrame(OUTPUT_TABLE, index=None)
     output.to_excel("output_table.xlsx", index=False)
